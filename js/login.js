732635834e6a5de84d3d07e207253baa6b4a1dc2
SamantaChirinos <104238787+SamantaChirinos@users.noreply.github.com>
2022-07-29 20:09:03 -0500

    Add files via upload
    Actualización del proyecto. Estructura del e-commerce

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const botonLogin = document.getElementById("login__boton");
+const email = document.getElementById("login__email");
+const contrasenha = document.getElementById("login__contrasenha");
+
+botonLogin.addEventListener("clic", function(){
+    
+});
